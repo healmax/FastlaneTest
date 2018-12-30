acbe25244da326db0972d631d28dd2fd660962f9
Vincent <healmax@gmail.com>
2018-12-30 12:06:08 +0800

    first change

---
+++ FastlaneTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastlaneTest
+//
+//  Created by healmax healmax on 2018/12/30.
+//  Copyright © 2018年 com.healmax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastlaneTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastlaneTest
+//
+//  Created by healmax healmax on 2018/12/30.
+//  Copyright © 2018年 com.healmax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
